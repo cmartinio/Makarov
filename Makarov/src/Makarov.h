d11ef90c90857918d221042be5dce68c5bb4273e
C Martin <fxgx99@gmail.com>
2020-07-28 14:24:22 -0400

    Update Makarov.h
    Working on a solution to fix spdlog compilation error caused by a third party library

--- Makarov/src/Makarov.h
+++ Makarov/src/Makarov.h
@@ -1,7 +1,8 @@
 #pragma once
 
 // For use by Makarov Applications
-
+// There has been some major changes to spdlog which is affecting the compilation process. 
+// I am working on a solution to fix that error in the near future. Hence the long silence.
 
 #include "Makarov/Application.h"
 #include "Makarov/Log.h"
